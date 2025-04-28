054d710d69d4f07e0523c492785e336f2532d4fc
Audric Andhika <audricc015@gmail.com>
2025-04-28 08:26:00 +0700

    Praktikum 1

--- Minggu11/Jobsheet11/app/Models/UserModel.php
+++ Minggu11/Jobsheet11/app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -50,5 +51,12 @@ class UserModel extends Authenticatable implements JWTSubject
     public function getRole()
     {
         return $this->level->level_kode;
+    }
+
+    public function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/'.$image),
+        );
     }
 }
